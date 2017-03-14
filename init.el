3afc73fc581294577e6676de993be5a7288c676c
Pang Tun Jiang <pang.tun.jiang@gmail.com>
2017-03-14 23:59:38 +0800

    Still havent' figured out el-get

--- init.el
+++ init.el
@@ -45,7 +45,7 @@
     ("#dc322f" "#cb4b16" "#b58900" "#546E00" "#B4C342" "#00629D" "#2aa198" "#d33682" "#6c71c4")))
  '(package-selected-packages
    (quote
-    (discover helm-company elpy evil-tabs projectile docker-tramp evil-text-object-python tramp-term company powershell use-package solarized-theme org-evil helm evil-surround evil-replace-with-register evil-numbers evil-magit evil-leader evil-commentary evil-args)))
+    (## el-get discover helm-company elpy evil-tabs projectile docker-tramp evil-text-object-python tramp-term company powershell use-package solarized-theme org-evil helm evil-surround evil-replace-with-register evil-numbers evil-magit evil-leader evil-commentary evil-args)))
  '(pos-tip-background-color "#073642")
  '(pos-tip-foreground-color "#93a1a1")
  '(smartrep-mode-line-active-bg (solarized-color-blend "#859900" "#073642" 0.2))
@@ -98,43 +98,6 @@
 
 
 
-
-
-
-
-
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
-(add-to-list 'package-archives '("elpy" . "http://jorgenschaefer.github.io/packages/"))
-; https://marmalade-repo.org/packages/#windowsinstructions
-(add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
-(when (< emacs-major-version 24)
-  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
-  )
-(package-refresh-contents)
-(package-initialize)
-
-(use-package el-get
-  :init
-  (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
-  :config
-  (add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
-  (el-get 'sync)
-  )
-
-(elpy-enable) ; TODO: configure elpy
-
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-
-(eval-when-compile
-  (require 'use-package))
-
-;; make sure we download when necessary
-(setq use-package-always-ensure t)
-
 ;; turn on line numbers
 (global-linum-mode 1)
 
@@ -143,6 +106,9 @@
 
 ;; autopairing
 (electric-pair-mode 1)
+
+;; autoindentation
+(electric-indent-mode 1)
 
 ; Remove toolbar
 (tool-bar-mode -1)
@@ -161,76 +127,121 @@
   (interactive)
   (insert (format-time-string current-date-time-format (current-time))))
 
-;; ;;evil-leader config
-;; (use-package evil-leader
-;;     :init
-;;     (add-to-list 'load-path "~/.emacs.d/packages/evil-leader")
-;;     :config
-;;     (global-evil-leader-mode)
-;;     (evil-leader/set-leader "<SPC>")
-;;     (evil-leader/set-key
-;; 	"<SPC>" 'helm-M-x
-;; 	"\\" 'magit-status
-;; 	"f"  'helm-find-files
-;; 	"t" 'insert-current-date-time
-;;     ))
+
+
+
+
+
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/")) ;
+(add-to-list 'package-archives '("elpy" . "http://jorgenschaefer.github.io/packages/"))
+; https://marmalade-repo.org/packages/#windowsinstructions
+(add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/")) ;
+(when (< emacs-major-version 24)
+  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+  )
+(package-refresh-contents)
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+
+;; make sure we download when necessary
+(setq use-package-always-ensure t)
+
+
+(use-package el-get
+  :init
+  (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+  :config
+  (add-to-list 'el-get-recipe-path "~/.emacs.d/el-get-user/recipes")
+  (el-get 'sync)
+  )
+
+(use-package elpy
+  :config
+  (elpy-enable); TODO: configure elpy
+  )
+
+;; ;; Example el-get-sources
+;; (setq el-get-sources
+;;       '(:name yasnippet
+;; 	      :type github
+;; 	      :pkgname "joaotavora/yasnippet"
+;; 	      :after (yas-global-mode 1)
+;; 	      )
+;;       )
+
+(use-package yasnippet
+  :config
+  (setq yas-snippet-dirs
+	'("~/.emacs.d/snippets") ;; personal snippets
+	)
+  (yas-global-mode 1)
+  )
 
 (use-package evil
     :init
     (setq evil-want-C-u-scroll t)
 
     (use-package evil-leader
-	:init
-	(add-to-list 'load-path "~/.emacs.d/packages/evil-leader")
-	:config
-	(global-evil-leader-mode)
-	(evil-leader/set-leader "<SPC>")
-	(evil-leader/set-key
-	    "<SPC>" 'helm-M-x
-	    "\\" 'magit-status
-	    "f"  'find-files
-	    "t" 'insert-current-date-time
-	))
+		:init
+		(add-to-list 'load-path "~/.emacs.d/packages/evil-leader")
+		:config
+		(global-evil-leader-mode)
+		(evil-leader/set-leader "<SPC>")
+		(evil-leader/set-key
+			"<SPC>" 'helm-M-x
+			"\\" 'magit-status
+			"f"  'find-files
+			"t" 'insert-current-date-time)
+		)
 
     :config
     (evil-mode 1)
 
     (use-package evil-surround
-	:config
-	(global-evil-surround-mode 1)
-	(setq-default evil-surround-pairs-alist (cons '(?~ . ("~" . "~"))
-						    evil-surround-pairs-alist)))
-    
+		:config
+		(global-evil-surround-mode 1)
+		(setq-default evil-surround-pairs-alist (cons '(?~ . ("~" . "~"))
+								evil-surround-pairs-alist)))
+
     (use-package evil-args
-	:config
-	;; bind evil-args text objects
-	(define-key evil-inner-text-objects-map "i" 'evil-inner-arg)
-	(define-key evil-outer-text-objects-map "a" 'evil-outer-arg)
-	;; bind evil-forward/backward-args
-	(define-key evil-normal-state-map "L" 'evil-forward-arg)
-	(define-key evil-normal-state-map "H" 'evil-backward-arg)
-	(define-key evil-motion-state-map "L" 'evil-forward-arg)
-	(define-key evil-motion-state-map "H" 'evil-backward-arg)
-	;; bind evil-jump-out-args
-	(define-key evil-normal-state-map "K" 'evil-jump-out-args)
-	)
+		:config
+		;; bind evil-args text objects
+		(define-key evil-inner-text-objects-map "i" 'evil-inner-arg)
+		(define-key evil-outer-text-objects-map "a" 'evil-outer-arg)
+		;; bind evil-forward/backward-args
+		(define-key evil-normal-state-map "L" 'evil-forward-arg)
+		(define-key evil-normal-state-map "H" 'evil-backward-arg)
+		(define-key evil-motion-state-map "L" 'evil-forward-arg)
+		(define-key evil-motion-state-map "H" 'evil-backward-arg)
+		;; bind evil-jump-out-args
+		(define-key evil-normal-state-map "K" 'evil-jump-out-args)
+		)
 
     (use-package evil-numbers
-	:config
-	(define-key evil-normal-state-map (kbd "C-a") 'evil-numbers/inc-at-pt)
-	(define-key evil-normal-state-map (kbd "C-x") 'evil-numbers/dec-at-pt)
-	)
+		:config
+		(define-key evil-normal-state-map (kbd "C-a") 'evil-numbers/inc-at-pt)
+		(define-key evil-normal-state-map (kbd "C-x") 'evil-numbers/dec-at-pt)
+		)
 
     (use-package evil-commentary)
     (use-package evil-replace-with-register)
     (use-package evil-text-object-python)
     (use-package evil-magit)
     (use-package evil-tabs
-	:init
-	(use-package elscreen)
-	:config
-	(global-evil-tabs-mode t)
-	)
+		:init
+		(use-package elscreen)
+		:config
+		(global-evil-tabs-mode t)
+		)
 
     (define-key global-map (kbd "C-f") 'universal-argument)
     (define-key universal-argument-map (kbd "C-u") nil)
@@ -239,8 +250,24 @@
     (eval-after-load 'evil-maps
 	'(progn
 	    (define-key evil-motion-state-map (kbd "C-f") nil)
-	    (define-key evil-motion-state-map (kbd "C-u") 'evil-scroll-up)))
-    )
+	    (define-key evil-motion-state-map (kbd "C-u") 'evil-scroll-up)
+	    )
+	)
+       ;; change mode-line color by evil state
+   (lexical-let ((default-color (cons (face-background 'mode-line)
+                                      (face-foreground 'mode-line)))
+		 )
+     (add-hook 'post-command-hook
+       (lambda ()
+         (let ((color (cond ((minibufferp) default-color)
+                            ((evil-insert-state-p) '("#e80000" . "#ffffff"))
+                            ((evil-emacs-state-p)  '("#444488" . "#ffffff"))
+                            ((buffer-modified-p)   '("#006fa0" . "#ffffff"))
+                            (t default-color))))
+           (set-face-background 'mode-line (car color))
+           (set-face-foreground 'mode-line (cdr color)))))
+     )
+   )
 
 ;; rebind <C-u> to intended behavior, otherwise defaults to universal-argument
 
