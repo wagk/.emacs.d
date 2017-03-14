659cc8b1c1ee5b356efe75ab834b534b410b6444
Pang Tun Jiang <pang.tun.jiang@autodesk.com>
2017-03-14 10:44:15 +0800

    removoving stuff

--- elpa/evil-replace-with-register-20161127.2159/evil-replace-with-register-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "evil-replace-with-register" "20161127.2159" "Port of vim plugin ReplaceWithRegister" '((evil "1.0.8")) :url "https://github.com/Dewdrops/evil-ReplaceWithRegister" :keywords '("evil" "plugin"))
