c96931643d41e5e3e85dca9671e5621b537d9f0a
Pang Tun Jiang <pang.tun.jiang@autodesk.com>
2017-07-10 10:44:48 +0800

    Fixing a bug where I accidentally delete use-package helm

--- config/config-helm.el
+++ config/config-helm.el
@@ -20,32 +20,38 @@
 (use-package swiper
   :ensure t
   :after ivy)
-("C-w" . evil-delete-backward-word)
-("S-SPC" . helm-select-action)
-("TAB" . helm-execute-persistent-action))
-:config
-(setq helm-recentf-fuzzy-match t
-      helm-locate-fuzzy-match nil ;; locate fuzzy is worthless
-      helm-M-x-fuzzy-match t
-      helm-buffers-fuzzy-matching t
-      helm-semantic-fuzzy-match t
-      helm-apropos-fuzzy-match t
-      helm-imenu-fuzzy-match t
-      helm-lisp-fuzzy-completion t
-      helm-completion-in-region-fuzzy-match t
-      helm-split-window-in-side-p t)
-(progn (helm-autoresize-mode)
-       (setq helm-autoresize-min-height 40 ;; these values are %
-             helm-autoresize-max-height 40))
-;; evil-related configuration
-(evil-leader/set-key
-  "<SPC>" 'helm-M-x
-  "S-<SPC>" 'helm-resume
-  "y" 'helm-show-kill-ring
-  "h" 'helm-apropos
-  "-" 'helm-find-files
-  "_" 'helm-mini
-  "b" 'helm-bookmarks))
+
+(use-package helm
+  :ensure t
+  :demand t
+  :after evil
+  :bind(:map helm-map
+             ("C-w" . evil-delete-backward-word)
+             ("S-SPC" . helm-select-action)
+             ("TAB" . helm-execute-persistent-action))
+  :config
+  (setq helm-recentf-fuzzy-match t
+        helm-locate-fuzzy-match nil ;; locate fuzzy is worthless
+        helm-M-x-fuzzy-match t
+        helm-buffers-fuzzy-matching t
+        helm-semantic-fuzzy-match t
+        helm-apropos-fuzzy-match t
+        helm-imenu-fuzzy-match t
+        helm-lisp-fuzzy-completion t
+        helm-completion-in-region-fuzzy-match t
+        helm-split-window-in-side-p t)
+  (progn (helm-autoresize-mode)
+         (setq helm-autoresize-min-height 40 ;; these values are %
+               helm-autoresize-max-height 40))
+  ;; evil-related configuration
+  (evil-leader/set-key
+    "<SPC>" 'helm-M-x
+    "S-<SPC>" 'helm-resume
+    "y" 'helm-show-kill-ring
+    "h" 'helm-apropos
+    "-" 'helm-find-files
+    "_" 'helm-mini
+    "b" 'helm-bookmarks))
 
 ;; TODO: when defining helm desckeys make sure a global binding is also presentw
 ;; C-h seems broken (We've been overwriting it to enable terminal backspace)
