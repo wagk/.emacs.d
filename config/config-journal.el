82c37ddacc3a16a9142bc64699016f6f48e1b7f2
Pang Tun Jiang <pang.tun.jiang@gmail.com>
2017-08-05 04:02:40 +0800

    The night's take

--- config/config-journal.el
+++ config/config-journal.el
@@ -5,9 +5,16 @@
 ;;; Code:
 (require 'config-package)
 (require 'config-org)
+(require 'config-evil)
 
 (use-package org-journal
   :ensure t
+  :config
+  (setq org-journal-dir (concat org-directory "/journal/")
+        org-journal-date-format "%d%m%Y")
+  (progn (require 'evil-leader)
+         (evil-leader/set-key
+           "d d" 'org-journal-new-entry))
   )
 
 (provide 'config-journal)
