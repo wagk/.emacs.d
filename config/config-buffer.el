7fbe9e325060f1f8ea0c240f7f99cf0b7c97765c
Pang Tun Jiang <pang.tun.jiang@autodesk.com>
2017-09-11 20:33:49 +0800

    solarized hl-todo

--- config/config-buffer.el
+++ config/config-buffer.el
@@ -11,12 +11,29 @@
 (use-package color-theme
   :ensure t)
 
+(defconst $solarized-dark-base03   "#002b36")
+(defconst $solarized-dark-base02   "#073642")
+(defconst $solarized-dark-base01   "#586e75")
+(defconst $solarized-dark-base00   "#657b83")
+(defconst $solarized-dark-base0    "#839496")
+(defconst $solarized-dark-base1    "#93a1a1")
+(defconst $solarized-dark-base2    "#eee8d5")
+(defconst $solarized-dark-base3    "#fdf6e3")
+(defconst $solarized-dark-yellow   "#b58900")
+(defconst $solarized-dark-orange   "#cb4b16")
+(defconst $solarized-dark-red      "#dc322f")
+(defconst $solarized-dark-magenta  "#d33682")
+(defconst $solarized-dark-violet   "#6c71c4")
+(defconst $solarized-dark-blue     "#268bd2")
+(defconst $solarized-dark-cyan     "#2aa198")
+(defconst $solarized-dark-green    "#859900")
+
 (use-package solarized-theme
   :ensure t
   :config
-  (setq  solarized-use-variable-pitch nil
-         solarized-distinct-fringe-background t
-         solarized-high-contrast-mode-line t))
+  (setq solarized-use-variable-pitch nil
+        solarized-distinct-fringe-background t
+        solarized-high-contrast-mode-line t))
 
 (use-package base16-theme
   :disabled
@@ -45,8 +62,14 @@
 (use-package hl-todo
   :ensure t
   :config
-  (add-to-list 'hl-todo-keyword-faces '("BUG" . "#dc322f"))
-  (add-to-list 'hl-todo-keyword-faces '("DEBUG" . "#d33682"))
+  (customize-set-variable 'hl-todo-keyword-faces
+                          `(("TODO"  . ,$solarized-dark-yellow)
+                            ("DEBUG" . ,$solarized-dark-magenta)
+                            ("BUG"   . ,$solarized-dark-red)
+                            ("DONE"  . ,$solarized-dark-green)
+                            ("NOTE"  . ,$solarized-dark-base1)
+                            ("HACK"  . ,$solarized-dark-violet)
+                            ("FIXME" . ,$solarized-dark-orange)))
   (global-hl-todo-mode))
 
 ;; https://github.com/alpaker/Fill-Column-Indicator
