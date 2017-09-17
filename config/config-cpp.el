9661c1fcc3a1b99d1e97020fc2355d8e8fd384db
Pang Tun Jiang <pang.tun.jiang@gmail.com>
2017-09-18 00:03:08 +0800

    removed electric pair for <>

--- config/config-cpp.el
+++ config/config-cpp.el
@@ -17,16 +17,18 @@
 ;; treat .h files as cpp files
 (add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
 
-;; add < > electric pairing
-(defvar $c++-electric-pairs '((?< . ?>))
-  "Additional electric pairs for c++")
+;; we don't electric pair <> because it interferes with << operators
 
-(defun $c++-mode-add-pairs ()
-  (setq-local electric-pair-pairs (append electric-pair-pairs
-                                          $c++-electric-pairs))
-  (setq-local electric-pair-text-pairs electric-pair-pairs))
+;; ;; add < > electric pairing
+;; (defvar $c++-electric-pairs '((?< . ?>))
+;;   "Additional electric pairs for c++")
 
-(add-hook 'c++-mode-hook #'$c++-mode-add-pairs)
+;; (defun $c++-mode-add-pairs ()
+;;   (setq-local electric-pair-pairs (append electric-pair-pairs
+;;                                           $c++-electric-pairs))
+;;   (setq-local electric-pair-text-pairs electric-pair-pairs))
+
+;; (add-hook 'c++-mode-hook #'$c++-mode-add-pairs)
 
 (provide 'config-cpp)
 
