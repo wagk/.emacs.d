a07dc21eded2156b33545215c937e8dfa3ceb86e
Pang Tun Jiang <pang.tun.jiang@autodesk.com>
2017-09-22 18:21:56 +0800

    python docstring indent things

--- config/config-python.el
+++ config/config-python.el
@@ -22,7 +22,24 @@
   (add-hook 'python-mode-hook #'(lambda ()
                                   (add-to-list 'company-backends 'company-jedi))))
 
+(use-package flycheck-mypy
+  :ensure t
+  :config
+  (add-hook 'python-mode-hook #'(lambda ()
+                                  (add-to-list 'flycheck-disabled-checkers 'python-flake8)
+                                  (add-to-list 'flycheck-disabled-checkers 'python-pylint)
+                                  (flycheck-mode))))
 
-(provide 'config-python)
+;; remove really dumb indentation rule when inside docstring
+;; NOTE: it appears that :inside-docstring isn't documented
+;; https://emacs.stackexchange.com/questions/26435/how-can-i-disable-indentation-rules-within-docstrings-in-python-mode
+(when (and (>= emacs-major-version 25)
+           (>= emacs-minor-version 1))
+  (defun $python-mode-noindent-docstring (&optional _previous)
+    (when (eq (car (python-indent-context)) :inside-docstring)
+      'noindent))
+  (advice-add 'python-indent-line :before-until #'$python-mode-noindent-docstring))
+
+  (provide 'config-python)
 
 ;;; config-python.el ends here
