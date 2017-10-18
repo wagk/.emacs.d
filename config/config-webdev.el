78643d984531efc68daf21b9610365954e61cf2c
Pang Tun Jiang <pang.tun.jiang@gmail.com>
2017-10-18 22:44:42 +0800

    turning on evil snipe again

--- config/config-webdev.el
+++ config/config-webdev.el
@@ -20,7 +20,7 @@
   (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
-  (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode)) )
+  (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode)))
 
 (use-package js2-mode
   :ensure t
