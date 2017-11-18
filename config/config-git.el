b2cde178b0d02cbb673b0a139dc088e29f46de38
Pang Tun Jiang <pang.tun.jiang@gmail.com>
2017-11-19 00:46:29 +0800

    cleaning up git fringe

--- config/config-git.el
+++ config/config-git.el
@@ -7,20 +7,20 @@
 (require 'config-evil)
 (require 'config-indent)
 
-(use-package evil-magit)
-
 (use-package magit
   :commands magit-status
   :init
   (evil-leader/set-key
     ", ," 'magit-status)
   :config
-  (require 'evil-magit)
   (eval-after-load 'aggressive-fill-paragraph
     '(add-hook 'git-commit-setup-hook 'aggressive-fill-paragraph-mode))
   (eval-after-load 'fill-column-indicator
     '(add-hook 'git-commit-setup-hook 'turn-on-fci-mode))
   )
+
+(use-package evil-magit
+  :after magit)
 
 ;; https://github.com/nonsequitur/git-gutter-plus
 (use-package git-gutter+
@@ -32,7 +32,7 @@
     "h s" 'git-gutter+-stage-hunks
     "h u" 'git-gutter+-revert-hunks
     "h p" 'git-gutter+-show-hunk)
-  (global-git-gutter+-mode 1))
+  (global-git-gutter+-mode))
 
 (use-package git-gutter-fringe+
   :after git-gutter+)
