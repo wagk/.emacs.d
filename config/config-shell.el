b455bd95972317c5447d45880fce0c215d5ffcbc
Pang Tun Jiang <pang.tun.jiang@autodesk.com>
2018-01-29 09:52:16 +0800

    Disable evil mode within shell interface modes

--- config/config-shell.el
+++ config/config-shell.el
@@ -13,21 +13,16 @@
 ;;   (add-to-list 'exec-path "c:/cygwin64/bin")
 ;;   )
 
+(add-hook 'comint-mode-hook 'turn-off-evil-mode)
+
 (use-package multi-term
   :after evil
   :init
   (evil-ex-define-cmd "te[rminal]" 'multi-term)
-  :config
-  ;; (cond ((or (eq system-type 'ms-dos)
-  ;;            (eq system-type 'windows-nt)) (setq multi-term-program "cmd"))
-  ;;       (t (setq multi-term-program "/bin/bash")))
-
-  (add-hook 'term-mode-hook 'turn-off-evil-mode)
   )
 
-(add-hook 'eshell-mode-hook 'turn-off-evil-mode)
-
 (use-package powershell)
+
 (provide 'config-shell)
 
 ;;; config-shell.el ends here
