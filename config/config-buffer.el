4e4434f3d094cc6b2412aed6b04c1094a827cfb8
Pang Tun Jiang <pang.tun.jiang@gmail.com>
2018-03-17 21:03:05 +0800

    moving utf8 pref to init because package might need it

--- config/config-buffer.el
+++ config/config-buffer.el
@@ -63,9 +63,6 @@
               tab-width         2
               auto-hscroll-mode t)
 
-;; buffer encoding systems
-(prefer-coding-system 'utf-8)
-
 ;; use optimised linum mode if we can
 (when (>= emacs-major-version 26)
   (global-display-line-numbers-mode))
