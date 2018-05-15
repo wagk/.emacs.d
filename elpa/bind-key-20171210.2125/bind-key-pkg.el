1fe935a87f137ef4a34e7f65812a317b03994bb8
Pang Tun Jiang <pang.tun.jiang@autodesk.com>
2018-05-15 17:46:16 +0800

    Updating

--- elpa/bind-key-20171210.2125/bind-key-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "bind-key" "20171210.2125" "A simple way to manage personal keybindings" 'nil :commit "35a5486c251686a0b779ce1f2952211a29f7139f" :url "https://github.com/jwiegley/use-package" :keywords '("keys" "keybinding" "config" "dotemacs"))
--- elpa/elfeed-20180508.1153/elfeed-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "elfeed" "20180508.1153" "an Emacs Atom/RSS feed reader"
-  '((emacs "24.3")))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
