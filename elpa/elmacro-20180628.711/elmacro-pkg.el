accb841d820fc4dfa9a6e43024d7cb4e603e3c16
Pang Tun Jiang <pang.tun.jiang@autodesk.com>
2018-07-04 15:04:24 +0800

    updating packages

---
+++ elpa/elmacro-20180628.711/elmacro-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "elmacro" "20180628.711" "Convert keyboard macros to emacs lisp" '((s "1.11.0") (dash "2.13.0")))
---
+++ elpa/evil-goggles-20180702.353/evil-goggles-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "evil-goggles" "20180702.353" "Add a visual hint to evil operations" '((emacs "24.4") (evil "1.0.0")))
