50db81339eaa59a30e9d64051fd9637174d32477
Pang Tun Jiang <pang.tun.jiang@autodesk.com>
2018-07-13 10:44:20 +0800

    Ripgrep and hylang

---
+++ elpa/hy-mode-20180702.1240/hy-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "hy-mode" "20180702.1240" "Major mode for Hylang" '((dash "2.13.0") (dash-functional "1.2.0") (s "1.11.0") (emacs "24")))
---
+++ elpa/ripgrep-20180323.920/ripgrep-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "ripgrep" "20180323.920" "Front-end for ripgrep, a command line search tool" 'nil)
