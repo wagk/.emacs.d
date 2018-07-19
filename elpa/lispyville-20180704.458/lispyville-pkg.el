0c11fc3703fb9db9b4397e2c082ca4d0fea37fb2
Pang Tun Jiang <pang.tun.jiang@autodesk.com>
2018-07-19 16:54:35 +0800

    Installing some parinfer things

---
+++ elpa/lispyville-20180704.458/lispyville-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "lispyville" "20180704.458" "A minor mode for integrating evil with lispy." '((lispy "0") (evil "1.2.12") (cl-lib "0.5") (emacs "24.4")))
