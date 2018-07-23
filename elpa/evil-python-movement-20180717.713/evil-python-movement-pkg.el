570039c3a2d22b7752a6d4ff8be3704a82dd33e3
Pang Tun Jiang <pang.tun.jiang@autodesk.com>
2018-07-23 18:44:50 +0800

    adding python movements for evil

---
+++ elpa/evil-python-movement-20180717.713/evil-python-movement-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "evil-python-movement" "20180717.713" "Port Neovim's python movement to Evil" '((emacs "25.1") (cl-lib "0.5") (dash "2.13.0") (evil "1.0") (s "1.12.0")))
