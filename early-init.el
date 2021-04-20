84dbde8b2f1673133d5e8fabb92e05fbcd699e03
Pang Tun Jiang <mail@pangt.dev>
2021-04-20 18:45:14 +0800

    general updates (I know it's a mess)

--- early-init.el
+++ early-init.el
@@ -1,4 +1,6 @@
-;;; init.el --- Bootstrap further configurations
+;;; init.el --- Bootstrap further configurations -*- no-byte-compile: t -*-
 ;;; Commentary:
 ;;; Code:
 (setq package-enable-at-startup nil)
+;; (when (boundp 'auto-compile-on-load-mode))
+  ;; auto-compile exists
