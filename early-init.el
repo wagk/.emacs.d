be2516d0fb0b7cd2bb206f06b152e5a44cf61b9f
Pang Tun Jiang <mail@pangt.dev>
2021-05-05 19:01:07 +0800

    updates

--- early-init.el
+++ early-init.el
@@ -2,5 +2,10 @@
 ;;; Commentary:
 ;;; Code:
 (setq package-enable-at-startup nil)
+
+(if (and (fboundp 'native-comp-available-p)
+         (native-comp-available-p))
+    (setq comp-deferred-compilation t))
+
 ;; (when (boundp 'auto-compile-on-load-mode))
   ;; auto-compile exists
