39a4793983a5178598ce466653c23bfce0d3cc6a
Pang Tun Jiang <pang.tun.jiang@gmail.com>
2021-06-11 20:38:05 +0800

    updates

--- init.el
+++ init.el
@@ -39,6 +39,10 @@
 (if (eq system-type "windows-nt")
     (set-w32-system-coding-system  'utf-8))
 
+(customize-set-variable 'frame-background-mode 'light)
+(with-eval-after-load 'solarized-theme
+ (load-theme 'solarized-light t))
+
 (defconst user-init-file
   (locate-user-emacs-file "init.el")
   "Points to init.el.")
@@ -202,6 +206,49 @@ recovery. Maybe eventually load dependencies and all that."
   (straight-check-all)
   (straight-prune-build))
 
+(defun my-init-solarized-color-variables ()
+  "Solarized 1.0.0beta2[a] Color Palette[8]
+| Color   |    |     |     | sRGB    |     |     |     | xterm | Terminal  | Usage                          |
+|---------+----+-----+-----+---------+-----+-----+-----+-------+-----------+--------------------------------|
+| Name    | L* | a*  | b*  | Hex     |   R |   G |   B |  Code | Name      |                                |
+|---------+----+-----+-----+---------+-----+-----+-----+-------+-----------+--------------------------------|
+| Base03  | 15 | −12 | −12 | #002b36 |   0 |  43 |  54 |   234 | brblack   | background tones (dark theme)  |
+| Base02  | 20 | −12 | −12 | #073642 |   7 |  54 |  66 |   235 | black     | background tones (dark theme)  |
+| Base01  | 45 | −07 | −07 | #586e75 |  88 | 110 | 117 |   240 | brgreen   | content tones                  |
+| Base00  | 50 | −07 | −07 | #657b83 | 101 | 123 | 131 |   241 | bryellow  | content tones                  |
+| Base0   | 60 | −06 | −03 | #839496 | 131 | 148 | 150 |   244 | brblue    | content tones                  |
+| Base1   | 65 | −05 | −02 | #93a1a1 | 147 | 161 | 161 |   245 | brcyan    | content tones                  |
+| Base2   | 92 | −00 | 10  | #eee8d5 | 238 | 232 | 213 |   254 | white     | background tones (light theme) |
+| Base3   | 97 | 00  | 10  | #fdf6e3 | 253 | 246 | 227 |   230 | brwhite   | background tones (light theme) |
+| Yellow  | 60 | 10  | 65  | #b58900 | 181 | 137 |   0 |   136 | yellow    | accent tones                   |
+| Orange  | 50 | 50  | 55  | #cb4b16 | 203 |  75 |  22 |   166 | brred     | accent tones                   |
+| Red     | 50 | 65  | 45  | #dc322f | 220 |  50 |  47 |   160 | red       | accent tones                   |
+| Magenta | 50 | 65  | −05 | #d33682 | 211 |  54 | 130 |   125 | magenta   | accent tones                   |
+| Violet  | 50 | 15  | −45 | #6c71c4 | 108 | 113 | 196 |    61 | brmagenta | accent tones                   |
+| Blue    | 55 | −10 | −45 | #268bd2 |  38 | 139 | 210 |    33 | blue      | accent tones                   |
+| Cyan    | 60 | −35 | −05 | #2aa198 |  42 | 161 | 152 |    37 | cyan      | accent tones                   |
+| Green   | 60 | −20 | 65  | #859900 | 133 | 153 |   0 |    64 | green     | accent tones                   |"
+  (dolist (col '((sol-base03  . "#002b36")
+                 (sol-base02  . "#073642")
+                 (sol-base01  . "#586e75")
+                 (sol-base00  . "#657b83")
+                 (sol-base0   . "#839496")
+                 (sol-base1   . "#93a1a1")
+                 (sol-base2   . "#eee8d5")
+                 (sol-base3   . "#fdf6e3")
+                 (sol-yellow  . "#b58900")
+                 (sol-orange  . "#cb4b16")
+                 (sol-red     . "#dc322f")
+                 (sol-magenta . "#d33682")
+                 (sol-violet  . "#6c71c4")
+                 (sol-blue    . "#268bd2")
+                 (sol-cyan    . "#2aa198")
+                 (sol-green   . "#859900")))
+   ;; TODO: set documentation string
+   (set (car col) (cdr col))))
+
+
+
 ;; (defun my-bootstrap-el-get ()
 ;;   (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 ;;   (unless (require 'el-get nil 'noerror)
@@ -219,6 +266,7 @@ recovery. Maybe eventually load dependencies and all that."
   ;; (bootstrap-quelpa)
   ;; (my-bootstrap-el-get)
   (bootstrap-use-package)
+  (my-init-solarized-color-variables)
 
   ;; Load core configuration that I can't work without. Everything
   ;; else gets shoved into config.org except these.
@@ -549,6 +597,12 @@ we're adding a custom function for it here."
     :custom-face
     (org-headline-done ((t (:strike-through t))))
     (org-checkbox ((t (:bold t :box nil))))
+    (org-block ((((background dark)) (:background ,sol-base02))
+                (((background light)) (:background ,sol-base2))))
+    (org-block-begin-line ((((background dark)) (:inherit org-meta-line :underline nil))
+                           (((background light)) (:inherit org-meta-line :underline nil))))
+    (org-block-end-line ((((background dark)) (:inherit org-meta-line :overline nil))
+                         (((background light)) (:inherit org-meta-line :overline nil))))
     :custom
     (org-list-description-max-indent
      5 "Ideally we should familiarize ourselves with adding a newline
@@ -621,7 +675,7 @@ we're adding a custom function for it here."
       (general-define-key
        :keymaps 'org-mode-map
        :states '(normal insert motion)
-       ;; "C-^" 'org-insert-heading-after-current
+        ;; "C-^" 'org-insert-heading-after-current
        "C-^" 'org-meta-return
        "\236" 'org-insert-todo-heading-respect-content)))
   ;; (with-eval-after-load 'org
@@ -766,7 +820,7 @@ we're adding a custom function for it here."
       (unless (f-exists-p custom)
         (f-touch custom))
       (setq custom-file custom)
-      (load custom-file)))
+      (load "" :noerror)))
 
   ;; Load local configuration variables, we do it here so that
   ;; local.el gets access to the "core" init loads
