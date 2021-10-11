98a3320cfbc660e2c71e1b74b6055068af898cc5
Pang Tun Jiang <pang.tun.jiang@gmail.com>
2021-10-11 21:52:40 +0800

    simplify org-todo-keywords since I'm no longer at adsk

--- init.el
+++ init.el
@@ -619,8 +619,9 @@ we're adding a custom function for it here."
     (org-default-notes-file "~/.todo")
     (org-M-RET-may-split-line nil)
     (org-return-follows-link t)
-    (org-todo-keywords '((sequence "TODO(t)" "ONGOING(o)" "BLOCKED(b@)"
-                                   "|" "DONE(d@)" "SKIPPED(s@)")))
+    ;; (org-todo-keywords '((sequence "TODO(t)" "ONGOING(o)" "BLOCKED(b@)"
+    ;;                                "|" "DONE(d@)" "SKIPPED(s@)")))
+    (org-todo-keywords '((sequence "TODO(t)" "|" "DONE(d)")))
     (org-enforce-todo-checkbox-dependencies
      nil "Sometimes we are able to skip dependencies as things happen")
     (org-enforce-todo-dependencies
