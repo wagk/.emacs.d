2cc7de602fe6eb124398637bf62f4085ac5370ae
Pang Tun Jiang <mail@pangt.dev>
2021-11-02 21:07:00 +0800

    set gc cons threshold back to sane values

--- early-init.el
+++ early-init.el
@@ -12,7 +12,7 @@
   'comp-deferred-compilation-deny-list
   'native-comp-deferred-compilation-deny-list)
 
-(setq gc-cons-threshold most-positive-fixnum
+(setq ;; gc-cons-threshold most-positive-fixnum
       garbage-collection-messages t)
 
 ;; (when (boundp 'auto-compile-on-load-mode))
