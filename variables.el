49db91c1cf1f03b70f6c11a177ee901966b6e481
Pang Tun Jiang <mail@pangt.dev>
2022-11-17 20:31:22 +0800

    config: deft: configuration
    Also move some stuff out into `variables.el`

---
+++ variables.el
@@ -0,0 +1,11 @@
+(defcustom --todo-file nil
+  "The path to the file with things to do on this machine."
+  :type 'file)
+
+(defcustom --done-file nil
+  "The path to the file with done things on this machine."
+  :type 'file)
+
+(defcustom --notes-folder nil
+  "The path to folder where notes are stored on this machine"
+  :type 'file)
