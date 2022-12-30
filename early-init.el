ac69cfd529b67949cef5e6b3e8306a66244b0017
Pang Tun Jiang <mail@pangt.dev>
2022-12-30 13:31:48 +0800

    early-init: Disable gc messages

--- early-init.el
+++ early-init.el
@@ -14,9 +14,3 @@
 (defvaralias
   'comp-deferred-compilation-deny-list
   'native-comp-deferred-compilation-deny-list)
-
-(setq ;; gc-cons-threshold most-positive-fixnum
-      garbage-collection-messages t)
-
-;; (when (boundp 'auto-compile-on-load-mode))
-  ;; auto-compile exists
