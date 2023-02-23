1d1dab16e9cd44ab71da0a6384b535910fb437f8
Pang Tun Jiang <pang.tun.jiang@gmail.com>
2023-02-23 23:10:52 +0800

    variables: Add a group to personal configs

--- variables.el
+++ variables.el
@@ -1,17 +1,21 @@
 (defcustom --todo-file nil
   "The path to the file with things to do on this machine."
-  :type 'file)
+  :type 'file
+  :group 'personal)
 
 (defcustom --done-file nil
   "The path to the file with done things on this machine."
-  :type 'file)
+  :type 'file
+  :group 'personal)
 
 (defcustom --diary-file nil
   "The path to the file with the record of work on this machine."
-  :type 'file)
+  :type 'file
+  :group 'personal)
 
 (defcustom --notes-folder nil
   "The path to folder where notes are stored on this machine"
-  :type 'file)
+  :type 'file
+  :group 'personal)
 
 (provide 'config::variables)
