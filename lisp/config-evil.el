77f27b85084acafc6c8d82fb02e160b73cbe401b
Pang Tun Jiang <mail@pangt.dev>
2023-02-28 11:41:04 +0800

    config-evil: reindent comment

--- lisp/config-evil.el
+++ lisp/config-evil.el
@@ -163,10 +163,9 @@
 (cl-defun --evil-consult-buffer (split-type)
   "Looks weird because we attempt to collect the buffer name before
   splitting the windows, in order to not have dangling windows if the
-  split is cancelled.
-`consult-buffer' doesn't return the buffer itself so we have to find
-the buffer name by pretending to be `consult--buffer-display', which
-gets called internally."
+  split is cancelled. `consult-buffer' doesn't return the buffer
+  itself so we have to find the buffer name by pretending to be
+  `consult--buffer-display', which gets called internally."
   (interactive)
   (let (selected-buffer)
     (cl-flet ((collect-buffer-name (buffer &optional _norecord)
