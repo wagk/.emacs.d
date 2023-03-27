a451d2a48df0a2ce7441ddcdea0bae9d92fc34b7
Pang Tun Jiang <mail@pangt.dev>
2023-03-28 01:59:56 +0800

    lisp: org: add `org-web-tools`

--- lisp/config-org.el
+++ lisp/config-org.el
@@ -229,4 +229,14 @@
              org-ql-refile
              org-ql-find-path))
 
+(use-package org-web-tools
+  :straight t
+  :commands
+  (org-web-tools-insert-link-for-url
+   org-web-tools-insert-web-page-as-entry
+   org-web-tools-read-url-as-org
+   org-web-tools-convert-links-to-page-entries
+   org-web-tools-archive-attach
+   org-web-tools-archive-view))
+
 (provide 'config-org)
