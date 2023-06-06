ad44cb381fcb322c2ef8f87796352d62562c629b
Pang Tun Jiang <mail@pangt.dev>
2023-06-06 11:10:28 +0800

    Reindent `config-evil.el`

--- lisp/config-evil.el
+++ lisp/config-evil.el
@@ -130,31 +130,31 @@
 (cl-defun --evil-ex-define-cmds-splits-and-tabs
     (command buffer-fn &optional tab)
   "Does split and vsplit, and also tab. BUFFER-FN should return a buffer
-Try moving to `--evil-ex-define-buffer-cmds' instead."
+   Try moving to `--evil-ex-define-buffer-cmds' instead."
   (require 'evil)
   (unless (stringp command)
     (warn "given command is not a string! Got %s" command)
-    (return))
+    (cl-return))
   (evil-ex-define-cmd command
                       `(lambda () (interactive)
                         (funcall-interactively #',buffer-fn)))
   (let ((split-command-name (concat "S" command)))
     (evil-ex-define-cmd split-command-name
                         `(lambda () (interactive)
-                           (call-interactively 'evil-window-split)
-                           (funcall-interactively #',buffer-fn))))
+                          (call-interactively 'evil-window-split)
+                          (funcall-interactively #',buffer-fn))))
   (let ((vsplit-command-name (concat "V" command)))
     (evil-ex-define-cmd vsplit-command-name
                         `(lambda () (interactive)
-                           (call-interactively 'evil-window-vsplit)
-                           (funcall-interactively #',buffer-fn))))
+                          (call-interactively 'evil-window-vsplit)
+                          (funcall-interactively #',buffer-fn))))
   (when tab
     (let ((new-tab-command-name (concat "T" command)))
       (evil-ex-define-cmd new-tab-command-name
                           `(lambda () (interactive)
-                             (require 'tab-bar)
-                             (let ((tab-bar-new-tab-choice ,tab))
-                               (tab-bar-new-tab)))))))
+                            (require 'tab-bar)
+                            (let ((tab-bar-new-tab-choice ,tab))
+                              (tab-bar-new-tab)))))))
 
 (--evil-ex-define-cmds-splits-and-tabs "init"
                                         'find-user-init-file
@@ -179,6 +179,7 @@ Try moving to `--evil-ex-define-buffer-cmds' instead."
                                         #'(lambda ()
                                             (switch-to-buffer "*Messages*"))
                                         "*Messages*")
+
 (cl-defun --pop-to-buffer-string-or-fn (buf)
   "Normalizes BUF into a buffer, returning the buffer value."
   (pop-to-buffer-same-window
@@ -191,20 +192,20 @@ Try moving to `--evil-ex-define-buffer-cmds' instead."
 (cl-defun --evil-ex-define-buffer-cmds
     (command buf &key no-split no-vsplit no-tab)
   "Create :s*, :v*, and :t* variants of COMMAND, as configured.
-- BUF should be a function that accepts no arguments and returns a
-  buffer, or a filepath.
-- NO-SPLIT, NO-VSPLIT, and NO-TAB causes the corresponding cmds to not
-  be created.
+   - BUF should be a function that accepts no arguments and returns a
+    buffer, or a filepath.
+   - NO-SPLIT, NO-VSPLIT, and NO-TAB causes the corresponding cmds to not
+    be created.
 
-Note that some edge cases aren't handled yet: I haven't thought of
-what to do when BUF is `\(dired ...\)' or similar.
+   Note that some edge cases aren't handled yet: I haven't thought of
+   what to do when BUF is `\(dired ...\)' or similar.
 
-There also appears to be issues when a quoted function is passed in
-\(like `\'ibuffer'\)."
+   There also appears to be issues when a quoted function is passed in
+   \(like `\'ibuffer'\)."
   (require 'evil)
   (unless (stringp command)
     (warn "given command is not a string! Got %s" command)
-    (return))
+    (cl-return))
   (evil-ex-define-cmd command
                       `(lambda () (interactive)
                         (--pop-to-buffer-string-or-fn ,buf)))
@@ -221,9 +222,9 @@ There also appears to be issues when a quoted function is passed in
   (unless no-tab
     (evil-ex-define-cmd (concat "T" command)
                         `(lambda () (interactive)
-                           (require 'tab-bar)
-                           (let ((tab-bar-new-tab-choice ,buf))
-                             (tab-bar-new-tab))))))
+                          (require 'tab-bar)
+                          (let ((tab-bar-new-tab-choice ,buf))
+                            (tab-bar-new-tab))))))
 
 (evil-ex-define-cmd "view" #'(lambda () (interactive) (read-only-mode 'toggle)))
 
