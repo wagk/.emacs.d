01ccf486175bfb40988a745f90217ca3bdd981ed
Pang Tun Jiang <mail@pangt.dev>
2023-07-06 14:08:06 +0800

    Remove some unused variables

--- lisp/config-org-capture-templates.el
+++ lisp/config-org-capture-templates.el
@@ -3,45 +3,4 @@
 
 (require 'evil)
 
-(when (not (bound-and-true-p --diary-file))
-  (warn "variable `--diary-file' not set!"))
-
-(with-eval-after-load 'org-capture
-  (with-eval-after-load 'doct
-    (setq org-capture-templates
-          (doct-add-to
-           org-capture-templates
-           '(("Add to today's diary"
-              :keys "today"
-              :empty-lines 1
-              :file --diary-file
-              :template "* %{todo-state} %? %^g"
-              :datetree t
-              :children
-              (("done"
-                :keys "done"
-                :todo-state "DONE")
-               ("todo"
-                :keys "todo"
-                :todo-state "TODO")
-               ("diary"
-                :keys "diary"
-                :template "* %?"
-                :todo-state nil))))))))
-
-(defun --capture-diary-entry ()
-  (interactive)
-  (require 'org-capture)
-  (org-capture nil "todaydiary"))
-
-(--evil-ex-define-cmds-splits-and-tabs "diary"
-                       #'(lambda () (interactive)
-                           (find-file --diary-file))
-				       --diary-file)
-
-(evil-ex-define-cmd "dd" #'--capture-diary-entry)
-
-(with-eval-after-load 'org-agenda
-  (setq org-agenda-files `(,--diary-file)))
-
 (provide 'config-org-capture-templates)
--- lisp/config-variables.el
+++ lisp/config-variables.el
@@ -2,26 +2,6 @@
   "A list of configuration variables that is needed from the local
   machine.")
 
-(defcustom --todo-file nil
-  "The path to the file with things to do on this machine."
-  :type 'file
-  :group 'personal)
-
-(defcustom --done-file nil
-  "The path to the file with done things on this machine."
-  :type 'file
-  :group 'personal)
-
-(defcustom --diary-file nil
-  "The path to the file with the record of work on this machine."
-  :type 'file
-  :group 'personal)
-
-(defcustom --notes-folder nil
-  "The path to folder where notes are stored on this machine"
-  :type 'file
-  :group 'personal)
-
 (defcustom --default-emacs-theme 'solarized-dark
   "The name of the theme that we want to load by default"
   :type 'symbol
