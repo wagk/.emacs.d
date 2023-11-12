a9b4c5f50ca4d9de3ffd1ae17125cd51ee3d69f4
Pang Tun Jiang <mail@pangt.dev>
2023-11-12 23:43:05 +0800

    Disable native compilation on windows
    In my experience, emacs is unable to find the internal native compiler and
    generate a lot of warnings

--- early-init.el
+++ early-init.el
@@ -3,12 +3,15 @@
 ;;; Code:
 (setq package-enable-at-startup nil)
 
+(when (eq system-type 'windows-nt)
+  (setq native-comp-speed -1)
+  (setq native-comp-enable-subr-trampolines nil))
+
 (unless (eq system-type 'windows-nt)
   (add-to-list 'default-frame-alist '(undecorated . t)))
 
 (when (and (fboundp 'native-comp-available-p)
            (native-comp-available-p))
-  (setq comp-deferred-compilation t)
   (setq native-comp-async-report-warnings-errors 'silent))
 
 (defvaralias
