2eb72da650d4cde0cb8767b11ba74e77a4e13f88
Pang Tun Jiang <mail@pangt.dev>
2024-01-29 18:35:02 +0800

    config, obsidian: Move more stuff to config-obsidian

--- lisp/config-obsidian.el
+++ lisp/config-obsidian.el
@@ -7,10 +7,10 @@
   "Personal Obsidian hacks")
 
 (defcustom config-obsidian-directory nil
-  "Obsidian directory"
+  "Location of Obsidian directory."
   :type 'directory)
 
-(cl-defun config-obsidian-find-file ()
+(cl-defun config-obsidian--select-file-name ()
   "Search `config-obsidian-directory' for files ending in `.md'."
   (interactive)
   (require 'dash)
@@ -21,9 +21,31 @@
                                                    config-obsidian-directory))
                            it)))
          (selected-file (--completing-read "File: " files)))
-    (find-file (file-name-concat config-obsidian-directory selected-file))))
+    (file-name-concat config-obsidian-directory selected-file)))
+
+;; (cl-defun config-obsidian-kill-list-item ()
+;;   (interactive)
+;;   (pcase-let ((`(,begin ,end ,_))
+;;               (markdown-cur-list-item-bounds))
+;;     (message "%s %s" begin end)))
+
+(cl-defun config-obsidian-find-file ()
+  "Opens a markdown file in `config-obsidian-directory'."
+  (interactive)
+  (find-file (config-obsidian--select-file-name)))
 
 (with-eval-after-load 'config-evil-helpers
   (--evil-define-splits "nn" #'config-obsidian-find-file))
+
+(with-eval-after-load 'org-capture
+  (require 'doct)
+  ;; Define capture template
+  (setq org-capture-templates
+        (doct `(("Markdown Capture"
+                 :keys "c"
+                 :type plain
+                 :file config-obsidian--select-file-name
+                 :template "\n# %?"))))
+  (evil-ex-define-cmd "nc" #'(lambda () (interactive) (org-capture nil "c"))))
 
 (provide 'config-obsidian)
