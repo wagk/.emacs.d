5105510729ddbf45cd209cb7668845ba74edb6dd
Pang T <mail@pangt.dev>
2024-02-03 02:05:50 +0800

    markdown-datetree: Initial commit

---
+++ lisp/markdown-datetree.el
@@ -0,0 +1,10 @@
+;;; markdown-datetree.el --- Datetrees. In markdown.  -*- lexical-binding: t; -*-
+
+;; Author: Pang Tun Jiang <mail@pangt.dev>
+;; Keywords: docs, tools,
+(require 'buttercup)
+(require 'markdown-mode)
+
+;; TODO (pangt): Parse headers for date
+
+(provide 'markdown-datetree)
