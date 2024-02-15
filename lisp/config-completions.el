a2bcc5bb728dc12ff643148fe22ca5fb6f412385
Pang T <mail@pangt.dev>
2024-02-15 23:31:22 +0800

    config, completion: Remove `cape-symbol`
    It doesn't seem to be defined when I load it emacs on windows?

--- lisp/config-completions.el
+++ lisp/config-completions.el
@@ -125,7 +125,6 @@
   (add-to-list 'completion-at-point-functions #'cape-dabbrev)
   (add-to-list 'completion-at-point-functions #'cape-file)
   (add-to-list 'completion-at-point-functions #'cape-history)
-  (add-to-list 'completion-at-point-functions #'cape-keyword)
-  (add-to-list 'completion-at-point-functions #'cape-symbol))
+  (add-to-list 'completion-at-point-functions #'cape-keyword))
 
 (provide 'config-completions)
