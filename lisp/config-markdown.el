b656b6a1dca1434aab51ea1301b920d3fc6deea4
Pang Tun Jiang <mail@pangt.dev>
2024-03-04 13:14:39 +0800

    config, markdown: Convert `config-markdown-directory` to list

--- lisp/config-markdown.el
+++ lisp/config-markdown.el
@@ -80,23 +80,17 @@
 
 (defcustom config-markdown-directory nil
   "Location of Markdown directory."
-  :type 'directory)
+  :type (list directory))
 
 (cl-defun config-markdown--select-file-name ()
   "Search `config-markdown-directory' for files ending in `.md'."
   (interactive)
   (require 'dash)
-  (let* ((files (--> config-markdown-directory
-                     (directory-files-recursively it "\\.md$")
-                     (-map #'(lambda (elem)
-                               (file-relative-name elem
-                                                   config-markdown-directory))
-                           it)))
-         (selected-file (--completing-read "File: " files)))
-    ;; make sure it's markdown
-    (unless (s-ends-with-p ".md" selected-file)
-      (setq selected-file (concat selected-file ".md")))
-    (file-name-concat config-markdown-directory selected-file)))
+  (let* ((files (mapcar
+                 (lambda (dir)
+                   (directory-files-recursively dir "\\.md$"))
+                 config-markdown-directory)))
+    (--completing-read "File: " (apply #'append files))))
 
 (cl-defun config-markdown-find-file ()
   "Opens a markdown file in `config-markdown-directory'."
