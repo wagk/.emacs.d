776527aa8d21124b303741d6b4ba5ff77064f34d
Pang Tun Jiang <mail@pangt.dev>
2024-03-14 14:46:09 +0800

    config, markdown-datetree: Stub out stuff and add tests

---
+++ lisp/markdown-datetree-tests.el
@@ -0,0 +1,13 @@
+;;; markdown-datetree-tests.el --- markdown-datetree test cases  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2024  Pang Tun Jiang
+
+;; Author: Pang Tun Jiang <mail@pangt.dev>
+;; Keywords: docs, tools
+
+(require 'markdown-datetree)
+
+
+
+
+(provide 'markdown-datetree-tests)
--- lisp/markdown-datetree.el
+++ lisp/markdown-datetree.el
@@ -2,9 +2,29 @@
 
 ;; Author: Pang Tun Jiang <mail@pangt.dev>
 ;; Keywords: docs, tools,
+
 (require 'buttercup)
 (require 'markdown-mode)
 
 ;; TODO (pangt): Parse headers for date
+
+(defconst markdown-datetree-header-regexp
+  (rx bol
+      (one-or-more "#")
+      (one-or-more whitespace)
+      (group-n 1 (one-or-more any))
+      eol)
+  "Matches the markdown header I use (the ones that start with \"#\"). Captures
+  as first group the contents of the header, so we can check the date.")
+
+(cl-defun markdown-datetree-parse-file (filename)
+  "Given FILENAME, parse it as a datetree.
+Return a list of datetree elements."
+  (with-temp-buffer
+    (insert-file-contents filename)
+    (goto-char (point-min))
+    (let (points)
+      (while (search-forward-regexp markdown-datetree-header-regexp nil t)
+        (push (line-beginning-position) points)))))
 
 (provide 'markdown-datetree)
