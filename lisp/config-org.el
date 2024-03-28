e791c18f4bde3ca8de077ecbb53f5ee1a4838c72
Pang Tun Jiang <mail@pangt.dev>
2024-03-28 15:20:14 +0800

    config: org: Move into `config-org.el`

--- lisp/config-org.el
+++ lisp/config-org.el
@@ -5,6 +5,7 @@
 ;;; org-mode
 
 (use-package org
+  :ensure t ;; use non-emacs version
   :demand t
   :commands (orgtbl-mode
              org-babel-load-file)
@@ -173,7 +174,43 @@
       (let ((document (org-element-interpret-data (org-element-parse-buffer))))
         (erase-buffer)
         (insert document)
-        (goto-char (point-min))))))
+        (goto-char (point-min)))))
+
+  (defun my-org-paste-clipboard-screenshot (&optional dir)
+    "Take a screenshot into a time stamped unique-named file in the
+     same directory as the org-buffer and insert a link to this file.
+
+Taken from `http://www.sastibe.de/2018/11/take-screenshots-straight-into-org-files-in-emacs-on-win10/`"
+    (interactive (list (read-directory-name "" "" "images")))
+    (require 'org)
+    (unless (equal system-type 'windows-nt)
+      (user-error "Implementation currently only works on windows, this is %s"
+                  system-type))
+    (let ((filename (concat
+                     (make-temp-name
+                      (concat (file-name-as-directory dir)
+                              (-> (buffer-file-name)
+                                  file-name-nondirectory
+                                  file-name-sans-extension)
+                              "_"
+                              (format-time-string "%Y-%m-%dT%H%M%S")))
+                     ".png")))
+      (unless (file-directory-p dir)
+        (make-directory dir))
+      (shell-command (concat "powershell -command \"Add-Type -AssemblyName System.Windows.Forms;if ($([System.Windows.Forms.Clipboard]::ContainsImage())) {$image = [System.Windows.Forms.Clipboard]::GetImage();[System.Drawing.Bitmap]$image.Save('"
+                             filename
+                             "',[System.Drawing.Imaging.ImageFormat]::Png); Write-Output 'clipboard content saved as file'} else {Write-Output 'clipboard does not contain image data'}\""))
+      (insert (concat "[[file:" (file-relative-name filename) "]]"))
+      (message "Image saved as %s" filename)
+      (org-display-inline-images)
+      filename))
+
+  (with-eval-after-load 'general
+    (general-define-key
+      :keymaps 'org-mode-map
+      :states '(normal)
+      :prefix my-default-evil-leader-key
+      "o p" 'my-org-paste-clipboard-screenshot)))
 
 (with-eval-after-load 'org-persist
   (customize-set-value 'org-persist-directory
@@ -181,6 +218,7 @@
 
 (use-package org-id
   :ensure nil
+  :after org
   :defer t
   :custom
   (org-id-ts-format "%s")
@@ -188,6 +226,7 @@
 
 (use-package evil-org
   :ensure (:host github :repo "Somelauw/evil-org-mode")
+  :after org
   :blackout t
   :preface
   (fset 'evil-redirect-digit-argument 'ignore)
@@ -222,7 +261,7 @@
                             calendar)))
 
 (use-package org-web-tools
-  
+  :ensure t
   :commands
   (org-web-tools-insert-link-for-url
    org-web-tools-insert-web-page-as-entry
@@ -230,5 +269,218 @@
    org-web-tools-convert-links-to-page-entries
    org-web-tools-archive-attach
    org-web-tools-archive-view))
+
+(use-package org-agenda
+  :ensure nil
+  :after org
+  :commands (org-todo-list
+             org-agenda-list
+             org-agenda-file-to-front
+             org-agenda)
+  :general
+  (:states 'motion
+   "|" 'org-agenda)
+  (:keymaps 'org-agenda-keymap
+   :states '(normal motion)
+   "g r" 'org-agenda-redo
+   "g t" nil
+   "g T" nil
+   "g l" 'org-agenda-log-mode
+   "g x" 'org-open-at-point-global
+   ;; Seems to be an unhandled case by evil-org
+   "RET" 'org-agenda-switch-to
+   [remap evil-write] 'org-save-all-org-buffers
+   [remap evil-save-modified-and-close] #'(lambda ()
+                                            (interactive)
+                                            (org-save-all-org-buffers)
+                                            (org-agenda-quit)))
+  :custom
+  (org-agenda-custom-commands '(("A" "TODOs and Agenda items"
+                                 ((alltodo "")
+                                  (agenda "")))))
+  (org-agenda-start-with-log-mode nil "Use `g l' instead")
+  (org-agenda-log-mode-items '(closed clock state))
+  (org-agenda-span 'month)
+  (org-agenda-window-setup 'current-window)
+  (org-agenda-skip-unavailable-files t)
+  (org-agenda-time-leading-zero t)
+  (org-agenda-prefix-format '((agenda . " %i %-12:c%?-12t%s %?b")
+                              (todo . " %i %-12:c%?-12b")
+                              (tags . " %i %-12:c")
+                              (search . " %i %-12:c")))
+  (org-agenda-breadcrumbs-separator "/")
+  :custom-face
+  (org-agenda-done ((t (:inherit org-agenda-done :strike-through t))))
+  (org-agenda-date-today ((t (:inherit org-agenda-date :overline t :bold t :inverse t))))
+  :hook
+  (org-agenda-after-show-hook . org-narrow-to-subtree)
+  :init
+  (--evil-ex-define-cmds-splits-and-tabs
+   "agenda"
+   #'(lambda () (interactive) (org-agenda nil "A"))
+   #'(lambda () (org-agenda nil "A")))
+
+  (cl-defun --run-with-local-idle-timer (secs repeat function &rest args)
+    "Like `run-with-idle-timer', but always runs in the `current-buffer'.
+   Cancels itself, if this buffer was killed.
+   Stolen from https://emacs.stackexchange.com/a/13275"
+   (let* (;; Chicken and egg problem.
+          (fns (make-symbol "local-idle-timer"))
+          (timer (apply 'run-with-idle-timer secs repeat fns args))
+          (fn `(lambda (&rest args)
+                 (if (not (buffer-live-p ,(current-buffer)))
+                     (cancel-timer ,timer)
+                   (with-current-buffer ,(current-buffer)
+                    (apply (function ,function) args))))))
+     (fset fns fn)
+     fn))
+  :config
+  (require 'evil-org-agenda)
+  (customize-set-value 'org-agenda-start-day "-1d"
+                       "For some reason this isn't being recognized
+                       and loaded in `:custom`")
+  (evil-org-agenda-set-keys)
+  (with-eval-after-load 'tab-bar
+    (general-define-key
+     :keymaps 'org-agenda-mode-map
+     :states '(motion)
+     "g t" 'tab-bar-switch-to-next-tab
+     "g T" 'tab-bar-switch-to-prev-tab))
+
+  (define-advice org-agenda-capture (:override () --consult-org-agenda-capture)
+    "Overrides `org-agenda-add' with a more consult-like interface"
+    (interactive)
+    (call-interactively '--org-capture-completing-read))
+
+  ;; override `org-agenda-diary-entry' to use `org-roam'
+  (general-define-key
+   :keymaps 'org-agenda-mode-map
+   :states '(motion)
+   "i" #'(lambda () (interactive)
+           (require 'org-roam)
+           (--org-roam-dailies-today))))
+
+;; https://github.com/alphapapa/org-super-agenda
+(use-package org-super-agenda
+  :after org-agenda
+  :ensure t
+  :commands org-super-agenda-mode
+  :hook (org-agenda-mode-hook . org-super-agenda-mode)
+  :general
+  (org-super-agenda-mode-map
+   "g t" nil
+   "g T" nil)
+  :custom
+  (org-super-agenda-groups
+   '((:auto-todo t)))
+  ;; '((:name "Blockers"
+  ;;    :todo "WAIT")
+  ;;   (:name "To Do"
+  ;;    :todo "TODO")))
+  ;; (:name "Personal Work"
+  ;;  :tag "personal"
+  ;;  :and (:not (:habit t)))
+  ;; (:name "Habits"
+  ;;  :habit t)))
+  ;; (:name "Unscheduled Work"
+  ;;  :not (:scheduled t))))
+  :config
+  ;; don't let org-super-agenda override evil bindings
+  ;; https://github.com/codygman/doom-emacs-literate-config/commit/bcd6ee115db58d12a05ff4aa9ba60f96d87b81ba
+  (setq org-super-agenda-header-map (make-sparse-keymap)))
+
+(use-package org-src
+  :after org
+  :ensure nil
+  :init
+  (defun my-evil-org-src-save-exit ()
+    (interactive)
+    (org-edit-src-save)
+    (org-edit-src-exit))
+  :general
+  (org-src-mode-map
+   [remap evil-write] 'org-edit-src-save
+   ;; doesn't seem to be working, the saving part at least
+   [remap evil-save-and-close] #'my-evil-org-src-save-exit
+   ;; doesn't seem to be working, the saving part at least
+   [remap evil-save-modified-and-close] #'my-evil-org-src-save-exit
+   [remap evil-quit] 'org-edit-src-abort))
+
+(use-package ob-async
+  :after org
+  :ensure t)
+
+(use-package ob-http
+  :ensure t
+  :after org
+  :config
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   (add-to-list 'org-babel-load-languages '(http . t))))
+
+(use-package org-fragtog
+  :after org
+  :ensure (:host github :repo "io12/org-fragtog")
+  :hook (org-mode-hook . org-fragtog-mode))
+
+(use-package org-clock
+  :ensure nil
+  :after org
+  :custom
+  (org-clock-clocked-in-display 'both)
+  (org-clock-persist 'history)
+  (org-clock-mode-line-total 'current)
+  (org-clock-out-remove-zero-time-clocks t)
+  (org-clock-history-length 35)
+  :general
+  (:states '(normal)
+   :prefix my-default-evil-leader-key
+   "c c" 'my-org-clocking
+   "c f" 'my-org-goto-clock)
+  :init
+  (defun my-org-goto-clock ()
+    (interactive)
+    (require 'org-clock)
+    (org-clock-goto (not (org-clocking-p))))
+  (evil-define-command my-org-clocking (&optional bang)
+    "Note that adding a ! means you mark the current task as the
+  default."
+    (interactive "<!>")
+    (require 'org-clock)
+    (cond
+     ((org-clocking-p) (org-clock-out))
+     ((and (eq major-mode 'org-mode) (org-at-heading-p))
+      (org-clock-in))
+     ((string-equal (buffer-name) "*Org Agenda*")
+      (org-agenda-clock-in))
+     (t (org-clock-in '(4))))
+    (when (or bang (not (marker-position org-clock-default-task)))
+      (org-clock-mark-default-task)))
+  (evil-ex-define-cmd "clock" #'my-org-clocking)
+  (evil-ex-define-cmd "clocking" #'my-org-goto-clock)
+  :config
+  (org-clock-persistence-insinuate)
+  (org-clock-load))
+
+(use-package org-appear
+  :after org
+  :ensure (:host github :repo "awth13/org-appear")
+  :commands (org-appear-mode)
+  :hook (org-mode-hook . org-appear-mode))
+
+(use-package valign
+  :ensure t
+  :after org
+  :hook (org-mode-hook . valign-mode))
+
+;; https://github.com/alphapapa/org-ql
+(use-package org-ql
+  :after org-super-agenda
+  :commands
+  (org-ql-search
+    org-ql-view
+    org-ql-view-sidebar
+    org-ql-view-recent-items
+    org-ql-sparse-tree))
 
 (provide 'config-org)
