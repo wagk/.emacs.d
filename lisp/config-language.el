76105daf2075d534bf5ed0fc8ea335d681fe3385
Pang T <mail@pangt.dev>
2024-03-30 00:41:02 +0800

    config: Migrate C++ stuff

--- lisp/config-language.el
+++ lisp/config-language.el
@@ -225,4 +225,93 @@ Lisp function does not specify a special indentation."
   :ensure t
   :commands (yaml-parse-string))
 
+(use-package cc-mode
+  :ensure nil
+  :general
+  (:states 'insert
+   :keymaps 'c-mode-base-map
+   "C-<return>" 'comment-indent-new-line)
+  (:keymaps '(c-mode-map c++-mode-map)
+    "<tab>" #'--c-indent-then-complete
+    "TAB" "<tab>")
+  :init
+  (cl-defun --c-indent-then-complete ()
+    (interactive)
+    (when (= 0 (c-indent-line-or-region))
+      (completion-at-point)))
+  :config
+  (add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
+  (add-to-list 'auto-mode-alist '("\\.hpp\\'" . c++-mode))
+
+  (setq-default c-basic-offset 4)
+
+  (cl-dolist (mode '(c-mode c++-mode))
+    (add-to-list 'c-default-style `(,mode . "k&r")))
+
+  (with-eval-after-load 'c-ts-mode
+    (setq c-ts-mode-indent-offset 4))
+
+  ;; We do not support =&&= (and hence also =||=) because of conflicts with
+  ;; r-value references.
+  (add-hook 'c-mode-common-hook
+            #'(lambda ()
+                (mapc (lambda (pair) (push pair prettify-symbols-alist))
+                      '(("!=" . #x2260)
+                        ("!" . #xac)
+                        ;; ("==" . #x2a75) ;; Not supported by Iosevka
+                        ;; ("->" . #x27f6) ;; Not supported by Iosevka
+                        ("->" . #x2192) ;; 1-char width version
+                        ("<=" . #x2264)
+                        (">=" . #x2265))))))
+;; ("&&" . #x2227)
+;; ("||" . #x2228)))))
+
+(use-package clang-format
+  :ensure (:host github :repo "emacsmirror/clang-format")
+  :commands (clang-format-region
+             clang-format-buffer
+             clang-format)
+  :general
+  (c++-mode-map
+   :states '(normal)
+   "C-c C-f" 'my-evil-clang-format)
+  (java-mode-map
+   :states '(normal)
+   "C-c C-f" 'my-evil-clang-format)
+  :init
+  (with-eval-after-load 'evil
+    (evil-define-operator my-evil-clang-format (beg end)
+      (require 'clang-format)
+      (clang-format beg end)))
+
+  (cl-defun toggle-clang-format-on-save ()
+    "Toggle clang-format-buffer on a per-buffer level."
+    (interactive)
+    (unless (boundp 'toggle-clang-format-on-save)
+      (setq-local toggle-clang-format-on-save nil))
+    (if (not toggle-clang-format-on-save)
+        (progn (add-hook 'before-save-hook 'clang-format-buffer t t)
+               (setq-local toggle-clang-format-on-save t)
+               (message "toggle-clang-format-on-save t"))
+      (remove-hook 'before-save-hook 'clang-format-buffer t)
+      (setq-local toggle-clang-format-on-save nil)
+      (message "toggle-clang-format-on-save nil")))
+  :custom
+  (clang-format-style-option "file"
+                             "read from .clang-format"))
+
+(use-package clang-format+
+  :after clang-format
+  :commands clang-format+-mode)
+
+;; Requires `clang` to be installed (perhaps unsurprisingly).
+(use-package clang-capf
+  :ensure t
+  :custom
+  (clang-capf-ignore-case t)
+  :config
+  (add-hook 'c-mode-hook
+            #'(lambda ()
+                (add-hook 'completion-at-point-functions #'clang-capf nil t))))
+
 (provide 'config-language)
