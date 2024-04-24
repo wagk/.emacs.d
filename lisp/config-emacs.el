75d59f0faa4bda28bef9299825616f50cd7da1cb
Pang Tun Jiang <mail@pangt.dev>
2024-04-24 12:55:27 +0800

    config: gdb: Stick transient into `with-eval-after-load` block
    For some reason `transient` isn't bundled with emacs?

--- lisp/config-emacs.el
+++ lisp/config-emacs.el
@@ -548,30 +548,31 @@
   :custom
   (gud-highlight-current-line t)
   :init
-  (transient-define-prefix --gdb ()
-    ["`gdb-mi' command dispatcher.\n"
-     ["Debugger"
-      ("RET" "Start debugging" (lambda () (interactive)
-                                 (require 'project)
-                                 (if-let* ((project (project-current t))
-                                           (default-directory (project-root project)))
-                                     (call-interactively 'gdb)
-                                   (gdb))))
-      ("g r" "Refresh" gud-refresh)]
+  (with-eval-after-load 'transient
+    (transient-define-prefix --gdb ()
+     ["`gdb-mi' command dispatcher.\n"
+      ["Debugger"
+       ("RET" "Start debugging" (lambda () (interactive)
+                                  (require 'project)
+                                  (if-let* ((project (project-current t))
+                                            (default-directory (project-root project)))
+                                      (call-interactively 'gdb)
+                                    (gdb))))
+       ("g r" "Refresh" gud-refresh)]
 
-     ["Point-based commands"
-      ("b" "Set breakpoint" gud-break)
-      ("t" "Set temporary breakpoint" gud-tbreak)
-      ("d" "Remove breakpoint" gud-remove)
-      ("p" "Evaluate expression" gud-remove)
-      ("u" "Execute to this line" gud-until)]
+      ["Point-based commands"
+       ("b" "Set breakpoint" gud-break)
+       ("t" "Set temporary breakpoint" gud-tbreak)
+       ("d" "Remove breakpoint" gud-remove)
+       ("p" "Evaluate expression" gud-remove)
+       ("u" "Execute to this line" gud-until)]
 
-     ["Repl manipulation"
-      ("f u" "Up frame" gud-up :transient t)
-      ("f d" "Down frame" gud-down :transient t)
-      ("n" "Step skipping functions" gud-next :transient t)
-      ("s" "Step" gud-step :transient t)
-      ("c" "Continue" gud-cont)]])
+      ["Repl manipulation"
+       ("f u" "Up frame" gud-up :transient t)
+       ("f d" "Down frame" gud-down :transient t)
+       ("n" "Step skipping functions" gud-next :transient t)
+       ("s" "Step" gud-step :transient t)
+       ("c" "Continue" gud-cont)]]))
 
   (with-eval-after-load 'evil
     (evil-ex-define-cmd "gdb" #'--gdb)
