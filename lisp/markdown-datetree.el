be487ffa77f5191bc108d4e7f137b88f49d48211
Pang Tun Jiang <mail@pangt.dev>
2024-05-13 14:45:45 +0800

    markdown-datetree: Insert very rudimentary markdown datetree support

--- lisp/markdown-datetree.el
+++ lisp/markdown-datetree.el
@@ -14,45 +14,108 @@
 
 (require 'rx)
 
-;; TODO (pangt): Parse headers for date
 ;; TODO (pangt): Assert that the hierarchy of year -> month -> day is correct
 ;; TODO (pangt): Provide helper functions to find, insert, and possibly fix
 ;; datetrees.
+;; TODO (pangt): Ensure that insertion is in sorted order. Currently the
+;; expectation is that we won't insert explicit dates (and only the current
+;; date, which is constantly increasing) and so the insertions will always be
+;; ordered. But that might not be true in the future and it will be good to
+;; consider that in the design anyway.
 
 (defgroup markdown-datetree nil
   "Functions for organizing a datetree within a markdown document.")
 
+(defconst markdown-datetree-root-heading-regexp "# Datetree")
+
 (defconst markdown-datetree-year-heading-regexp
-  (rx (1+ ?#) " " (= 4 digit) eol))
+  (rx "## " (group-n 1 (= 4 digit)) eol))
+
+(cl-defun markdown-datetree-specific-year-heading-regexp (year)
+  (cl-assert year)
+  (cl-assert (stringp year))
+  (rx "## " (literal year) eol))
 
 (defconst markdown-datetree-month-heading-regexp
-  (rx (1+ ?#) " " (= 4 digit) "-" (in "0-1") digit eol))
+  (rx "### " (= 4 digit) "-" (in "0-1") digit eol))
+
+(cl-defun markdown-datetree-specific-month-heading-regexp (year month)
+  (cl-assert year)
+  (cl-assert (stringp year))
+  (cl-assert month)
+  (cl-assert (stringp month))
+  (rx "### " (literal year) "-" (literal month) eol))
 
 (defconst markdown-datetree-day-heading-regexp
-  (rx (1+ ?#) " " (= 4 digit) "-" (in "0-1") digit "-" (in "0-3") digit eol))
+  (rx "#### " (= 4 digit) "-" (in "0-1") digit "-" (in "0-3") digit eol))
 
-(cl-defun markdown-datetree-go-to-date (&optional &key no-create)
-  "Within the buffer, tries to find the heading for today's date.")
+(cl-defun markdown-datetree-specific-day-heading-regexp (day)
+  (cl-assert day)
+  (cl-assert (stringp day))
+  (rx "#### " (= 4 digit) "-" (in "0-1") digit "-" (in "0-3") digit eol))
 
-(cl-defun markdown-datetree--buffer-collect-headings (buffer)
-  "Given BUFFER, collect all headings.
-Collect:
-- The point
-- The level
-- The text"
-  (with-current-buffer buffer
-    (let (points)
-      (save-excursion
-        (goto-char (point-min))
-        (while (search-forward-regexp config-markdown-header-regexp nil t)
-          (setq points (append points
-                               `((:point ,(line-beginning-position)
-                                  :level ,(length (match-string 1))
-                                  ;; might not be a date!
-                                  :header ,(match-string 2)))))))
-      points)))
+(cl-defun markdown-datetree-specific-date-heading-regexp (year month day)
+  (cl-assert year)
+  (cl-assert (stringp year))
+  (cl-assert month)
+  (cl-assert (stringp month))
+  (cl-assert day)
+  (cl-assert (stringp day))
+  (rx "#### " (literal year) "-" (literal month) "-" (literal day) eol))
 
-(cl-defun markdown-datetree-p (buffer)
-  (let ((headings (markdown-datetree--buffer-collect-headings buffer)))))
+(cl-defun markdown-datetree-go-to-datetree ()
+  "In the buffer, find or create a level one heading 'Datetree'.
+Moves the point to the end of the datetree root header."
+  (goto-char (point-min))
+  (unless (re-search-forward markdown-datetree-root-heading-regexp nil t)
+    (goto-char (point-max))
+    (insert "# Datetree"))
+  (end-of-line))
+
+(cl-defun markdown-datetree-go-to-year (&optional year)
+  "In the buffer, find or create the year header.
+Moves the point to the end of the year header."
+  (require 'outline)
+  (markdown-datetree-go-to-datetree)
+  (let ((year (if year (number-to-string year)
+                (format-time-string "%Y"))))
+    (unless (-> year
+                (markdown-datetree-specific-year-heading-regexp)
+                (re-search-forward nil t))
+      (outline-end-of-subtree)
+      (newline)
+      (insert (concat "## " year)))))
+
+(cl-defun markdown-datetree-go-to-month (&optional year month)
+  "In the buffer, find or create the month header.
+Moves the point to the end of the month header."
+  (require 'outline)
+  (markdown-datetree-go-to-year year)
+  (let ((month (if month (format "%02d" month)
+                (format-time-string "%m")))
+        (year (if year (number-to-string year)
+                (format-time-string "%Y"))))
+    (unless (-> (markdown-datetree-specific-month-heading-regexp year month)
+                (re-search-forward nil t))
+      (outline-end-of-subtree)
+      (newline)
+      (insert (concat "### " year "-" month)))))
+
+(cl-defun markdown-datetree-go-to-day (&optional year month day)
+  "In the buffer, find or create the day header.
+Moves the point to the end of the day header."
+  (require 'outline)
+  (markdown-datetree-go-to-month year month)
+  (let ((day (if day (format "%02d" day)
+               (format-time-string "%d")))
+        (month (if month (format "%02d" month)
+                 (format-time-string "%m")))
+        (year (if year (number-to-string year)
+                (format-time-string "%Y"))))
+    (unless (-> (markdown-datetree-specific-date-heading-regexp year month day)
+                (re-search-forward nil t))
+      (outline-end-of-subtree)
+      (newline)
+      (insert (concat "#### " year "-" month "-" day)))))
 
 (provide 'markdown-datetree)
