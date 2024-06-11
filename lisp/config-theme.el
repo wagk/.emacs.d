da65367060e8a5d00a23f16e9d0d37562c363348
Pang Tun Jiang <mail@pangt.dev>
2024-06-11 15:34:57 +0800

    config: markdown: Update faces for metadata
    Since git commits uses markdown, this means that the commit header is treated
    like a metadata keypair (especially with the way I write it).
    
    This means that this face is sort of fucked, since as a commit header I want it
    to be a bit flashy, but for frontmatter metadata I want it to be as muted as
    possible.
    
    It's likely that there will be additional changes to this in the future.

--- lisp/config-theme.el
+++ lisp/config-theme.el
@@ -415,6 +415,12 @@
                       :inherit 'nano-salient))
 
 (with-eval-after-load 'markdown-mode
+  (set-face-attribute 'markdown-metadata-key-face nil
+                      :weight 'semi-light
+                      :inherit 'nano-default)
+  (set-face-attribute 'markdown-metadata-value-face nil
+                      :weight 'semi-light
+                      :inherit 'nano-default)
   (set-face-attribute 'markdown-highlighting-face nil
                       :foreground 'unspecified
                       :background 'unspecified
