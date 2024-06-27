994fc96d28ff3e31c51bb6b0018049616259dd2a
Pang Tun Jiang <mail@pangt.dev>
2024-06-27 16:39:41 +0800

    config: git: Downcase `status`

--- lisp/config-git.el
+++ lisp/config-git.el
@@ -116,7 +116,7 @@ assume # starts a comment."
   :config
   (with-eval-after-load 'transient
     (transient-replace-suffix 'magit-dispatch #'magit-status-quick
-      (list "j" "Show Status" #'magit-status)))
+      (list "j" "Show status" #'magit-status)))
   (define-advice magit-log-propertize-keywords
       (:override (_rev msg) handle-conventional-commits)
     "https://github.com/magit/magit/issues/4027#issuecomment-1372397053"
