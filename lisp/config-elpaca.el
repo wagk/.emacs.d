953e1b0d26751af0d8196818ca3eb914dcc57d08
Pang Tun Jiang <TunJiang.Pang@squarepoint-capital.com>
2025-01-20 11:26:57 +0800

    config: elpaca: Bump to 0.9

--- lisp/config-elpaca.el
+++ lisp/config-elpaca.el
@@ -1,11 +1,10 @@
 ;; -*- lexical-binding: nil -*-
-
-(defvar elpaca-installer-version 0.8)
+(defvar elpaca-installer-version 0.9)
 (defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
 (defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
 (defvar elpaca-order '(elpaca :repo "https://github.com/progfolio/elpaca.git"
-                              :ref nil :depth 1
+                              :ref nil :depth 1 :inherit ignore
                               :files (:defaults "elpaca-test.el" (:exclude "extensions"))
                               :build (:not elpaca--activate-package)))
 (let* ((repo  (expand-file-name "elpaca/" elpaca-repos-directory))
@@ -37,7 +36,6 @@
     (elpaca-generate-autoloads "elpaca" repo)
     (load "./elpaca-autoloads")))
 
-(add-hook 'after-init-hook #'elpaca-process-queues)
 
 (when (eq system-type 'windows-nt)
   (setq elpaca-queue-limit 20)
@@ -47,6 +45,7 @@
 ;; (macroexpansion within let-bindings) that makes me hesitant to move this into
 ;; a function
 
+(add-hook 'after-init-hook #'elpaca-process-queues)
 (elpaca `(,@elpaca-order))
 
 (elpaca-wait)
