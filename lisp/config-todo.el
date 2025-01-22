4f0e0cc51b2553b7206b66d7ed86d90f2c2232ab
Pang T <mail@wagk.org>
2025-01-23 02:04:25 +0800

    config-todo: Implement a dirt-simple TODO tracker

--- lisp/config-todo.el
+++ lisp/config-todo.el
@@ -1,1 +1,52 @@
+;; todo
+
+;; TODO (pangt): Completion
+;; TODO (pangt): Decide syntax
+
+(defcustom config-todo-file "~/TODO"
+  "TODO file"
+  :group 'config-todo
+  :type 'file)
+
+(defvar-keymap config-todo-keymap
+  "C-c C-c" #'config-todo-do)
+
+(cl-defun config-todo--toggle-done ()
+  (save-excursion
+    (beginning-of-line)
+    (if (looking-at "x ")
+        (delete-char 2)
+      (insert "x "))))
+
+(cl-defun config-todo-do ()
+  "toggles between todo and done states (an x)"
+  (interactive)
+  (save-excursion
+    (cond
+     ;; ((use-region-p) (progn
+     ;;                   (message "range %s" evil-ex-range)
+     ;;                   (goto-char (region-beginning))
+     ;;                   (while (< (point) (region-end))
+     ;;                     (config-todo--toggle-done)
+     ;;                     (forward-line))
+     ;;                   (deactivate-mark)))
+     (t (config-todo--toggle-done)))))
+
+(cl-defun config-todo--prefix-date-on-newline ()
+  "Inserts a timestamp into every new line"
+  (when (and (bolp) (eolp))
+    (beginning-of-line)
+    (insert (format-time-string "%F "))
+    (end-of-line)))
+
+(cl-defun config-todo-open ()
+  "Goes to `config-todo-file'"
+  (interactive)
+  (find-file config-todo-file)
+  (add-hook 'post-command-hook #'config-todo--prefix-date-on-newline nil t)
+  (use-local-map config-todo-keymap))
+
+(with-eval-after-load 'config-evil-helpers
+  (--evil-define-splits "dd" #'config-todo-open))
+
 (provide 'config-todo)
