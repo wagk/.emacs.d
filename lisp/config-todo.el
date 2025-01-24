e7ae9e1836376e368238762fe4c03ac8c4b823d6
Pang T <mail@wagk.org>
2025-01-25 03:44:45 +0800

    config-todo: Trial out a proper major-mode, and sorting

--- lisp/config-todo.el
+++ lisp/config-todo.el
@@ -1,20 +1,70 @@
 ;; todo
 
-;; TODO (pangt): Completion
+;; TODO (pangt): Completion, keywords at least
 ;; TODO (pangt): Decide syntax
+;; every line is a todo, or a done
+;; `--'s separates the todo and the done context
+;; keywords can be foo:bar or :bar or bar: or foo:bar:baz (?)
+;; TODO (pangt): font-lock strikethrough for dones
 
 (defcustom config-todo-file "~/TODO"
   "TODO file"
   :group 'config-todo
   :type 'file)
 
+;; not sure if we'll need this
+(defcustom config-todo-done-file "~/DONE"
+  "DONE file"
+  :group 'config-todo
+  :type 'file)
+
 (defvar-keymap config-todo-keymap
   "C-c C-c" #'config-todo-do
-  "C-c C-s" #'(lambda ()
-                (interactive)
-                (require 'evil)
-                (let ((sort-fold-case t))
-                  (evil-ex-sort (point-min) (point-max)))))
+  "C-c C-s" #'config-todo-organize)
+
+(cl-defun config-todo-organize ()
+  "Normalize each line and then sort buffer."
+  (interactive)
+  (config-todo--normalize-every-line)
+  (delete-duplicate-lines (point-min) (point-max) nil nil nil t)
+  (config-todo--sort-todos))
+
+(cl-defun config-todo--sort-todos ()
+  (let* ((sort-fold-case t)
+         (done-rx (rx line-start "x"))
+         (done-point))
+    ;; sort lines in buffer
+    (sort-lines nil (point-min) (point-max))
+    ;; try to find a done task
+    (setq done-point (save-excursion
+                       (goto-char (point-min))
+                       (when (re-search-forward done-rx nil t)
+                         (match-beginning 0))))
+    (if done-point
+        ;; reverse sort done and not-done lines separately
+        (progn
+          (with-restriction (point-min) done-point
+            (sort-lines t (point-min) (point-max)))
+          (with-restriction done-point (point-max)
+            (sort-lines t (point-min) (point-max))))
+      ;; otherwise reverse the sort unilaterally
+      (sort-lines t (point-min) (point-max)))))
+
+(cl-defun config-todo--normalize-line (line)
+  "Takes in a string that is a line, and returns a string that is a line."
+  (string-trim line))
+
+(cl-defun config-todo--normalize-every-line ()
+  (save-excursion
+    (goto-char (point-min))
+    (while (not (eobp))
+      (let* ((line (buffer-substring-no-properties
+                    (line-beginning-position)
+                    (line-end-position)))
+             (line (config-todo--normalize-line line)))
+        (delete-region (line-beginning-position) (line-end-position))
+        (insert line))
+      (forward-line))))
 
 (cl-defun config-todo--toggle-done ()
   (save-excursion
@@ -48,10 +98,16 @@
   "Goes to `config-todo-file'"
   (interactive)
   (find-file config-todo-file)
-  (add-hook 'post-command-hook #'config-todo--prefix-date-on-newline nil t)
-  (use-local-map config-todo-keymap))
+  (config-todo-mode))
 
 (with-eval-after-load 'config-evil-helpers
   (--evil-define-splits "dd" #'config-todo-open))
+
+;; https://www.gnu.org/software/emacs/manual/html_node/elisp/Major-Mode-Conventions.html
+(define-derived-mode config-todo-mode text-mode "config-todo"
+  "line-based TODO text file"
+  (use-local-map config-todo-keymap)
+  (add-hook 'post-command-hook #'config-todo--prefix-date-on-newline nil t))
+
 
 (provide 'config-todo)
