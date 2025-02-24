c9879d6b8354497170d40b534243350058cdf572
Pang T <mail@pangt.dev>
2025-02-24 23:43:44 +0800

    early-config: Disable blocks that are no longer necessary

--- early-init.el
+++ early-init.el
@@ -3,24 +3,24 @@
 ;;; Code:
 (setq package-enable-at-startup nil)
 
-(when (boundp 'package-install-upgrade-built-in)
-  (setq package-install-upgrade-built-in t))
+;; (when (bound-and-true-p 'package-install-upgrade-built-in)
+;;   (setq package-install-upgrade-built-in t))
 
-(when (eq system-type 'windows-nt)
-  (setq native-comp-speed -1)
-  (setq no-native-compile t)
-  (setq native-comp-enable-subr-trampolines nil)
-  (setq native-comp-jit-compilation nil))
+;; (when (eq system-type 'windows-nt)
+;;   (setq native-comp-speed -1)
+;;   (setq no-native-compile t)
+;;   (setq native-comp-enable-subr-trampolines nil)
+;;   (setq native-comp-jit-compilation nil))
 
 (unless (eq system-type 'windows-nt)
   (add-to-list 'default-frame-alist '(undecorated . t)))
 
-(when (and (fboundp 'native-comp-available-p)
-           (native-comp-available-p))
-  (setq native-comp-async-report-warnings-errors 'silent)
-  (when (boundp 'native-comp-always-compile)
-    (setq native-comp-always-compile t)))
+;; (when (and (fboundp 'native-comp-available-p)
+;;            (native-comp-available-p))
+;;   (setq native-comp-async-report-warnings-errors 'silent)
+;;   (when (boundp 'native-comp-always-compile)
+;;     (setq native-comp-always-compile t)))
 
-(defvaralias
-  'comp-deferred-compilation-deny-list
-  'native-comp-deferred-compilation-deny-list)
+;; (defvaralias
+;;   'comp-deferred-compilation-deny-list
+;;   'native-comp-deferred-compilation-deny-list)
