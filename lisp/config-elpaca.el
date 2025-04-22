4f2a6c96f5f9c050c738d7c6b86f04c2a89f70c9
Pang T <mail@pangt.dev>
2025-04-22 23:17:43 +0800

    config: elpaca: Bump to 0.11

--- lisp/config-elpaca.el
+++ lisp/config-elpaca.el
@@ -1,5 +1,5 @@
 ;; -*- lexical-binding: nil -*-
-(defvar elpaca-installer-version 0.10)
+(defvar elpaca-installer-version 0.11)
 (defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
 (defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
@@ -34,7 +34,7 @@
   (unless (require 'elpaca-autoloads nil t)
     (require 'elpaca)
     (elpaca-generate-autoloads "elpaca" repo)
-    (load "./elpaca-autoloads")))
+    (let ((load-source-file-function nil)) (load "./elpaca-autoloads"))))
 
 (when (eq system-type 'windows-nt)
   (setq elpaca-queue-limit 25)
