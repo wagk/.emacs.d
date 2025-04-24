a8944a194aaefab0bd18365ca22609dfb31f8c24
Pang T <mail@pangt.dev>
2025-04-24 23:05:16 +0800

    config: completions: Add consult-register to :fr

--- lisp/config-completions.el
+++ lisp/config-completions.el
@@ -128,6 +128,7 @@
   (evil-ex-define-cmd "ii"  'consult-imenu-multi)
   (evil-ex-define-cmd "fp"  'consult-yank-from-kill-ring) ;; p for paste
   (evil-ex-define-cmd "ff"  'consult-line)
+  (evil-ex-define-cmd "fr"  'consult-register)
   (evil-ex-define-cmd "fl" '(lambda () (interactive)
                               (consult-line (thing-at-point 'symbol))))
   (evil-ex-define-cmd "fa"  'consult-line-multi)
